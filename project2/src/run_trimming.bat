8066f6e865d1d89a416abc091a277ebda70f5885
Gustavo_Miguel <g236875@dac.unicamp.br>
2026-04-19 16:05:46 -0300

    Removed scripts dir(?) moved to src/ | src/README.md att | create main script

--- project2/src/run_trimming.bat
+++ project2/src/run_trimming.bat
@@ -1,20 +1,26 @@
 @echo off
 cd /d C:\bio\Trimmomatic
 
+set "PROJECT_ROOT=%~dp0..\.."
+set "RAW_DIR=%PROJECT_ROOT%\qc\raw"
+set "TRIMMED_DIR=%PROJECT_ROOT%\qc\trimmed"
+set "TRIMMOMATIC_JAR="
+set "ADAPTERS="
+
 echo =========================
 echo Iniciando TRIMMING
 echo =========================
 
-for %%f in (C:\schisto\raw\*.fastq.gz) do (
+for %%f in ("%RAW_DIR%\*.fastq.gz") do (
 
     echo -------------------------
     echo Processando %%~nxf
     echo -------------------------
 
-    java -jar trimmomatic-0.40.jar SE ^
+    java -jar "%TRIMMOMATIC_JAR%" SE ^
     "%%f" ^
-    "C:\schisto\trimmed\%%~nf_trimmed.fastq.gz" ^
-    ILLUMINACLIP:adapters/TruSeq3-SE.fa:2:30:10 ^
+    "%TRIMMED_DIR%\%%~nf_trimmed.fastq.gz" ^
+    ILLUMINACLIP:"%ADAPTERS%":2:30:10 ^
     LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36
 
 )
@@ -22,4 +28,4 @@ for %%f in (C:\schisto\raw\*.fastq.gz) do (
 echo =========================
 echo TRIMMING FINALIZADO
 echo =========================
-pause
+
